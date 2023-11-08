2d4b933e5e6ffc6a7429b3650a7ab04f59df7261
Livia <livialuciani93@gmail.com>
2023-11-08 17:43:39 +0100

    add

--- src/page/home/home.jsx
+++ src/page/home/home.jsx
@@ -19,7 +19,8 @@ export default function Home() {
         <div className={Style.container1}>
             <Clock className={Style.clock} live={true} hourMarkFormat="number" mode="dark"  />
             <p className={Style.text}>
-            Mi presento sono Livia, e sono sviluppatrice web. <br/> Negli ultimi anni ho coltivato una grande passione per l’ informatica tramandata dalla mia famiglia. <br/>Per lungo tempo ho lavorato nel campo della ristorazione, <br/> 
+            Mi presento sono Livia, e sono sviluppatrice web. <br /> Negli ultimi anni ho coltivato una grande passione per l’ informatica tramandata dalla mia famiglia. <br />
+            Per lungo tempo ho lavorato nel campo della ristorazione, <br /> 
             diverse esperienze mi hanno insegnato la precisione, la puntualità e la determinazione, 
             oltre alla conoscenza dei vini e dell&apos;alta ristorazione.<br/> 
             Ma a 30 anni ho capito che volevo assecondare questo mio bisogno che è la tecnologia. <br/>
