a0ef8215c8e1549dacdd1500d0246b7f09e6e9a2
Livia <livialuciani93@gmail.com>
2024-02-27 22:22:44 +0100

    modifiche

--- src/page/home/home.jsx
+++ src/page/home/home.jsx
@@ -1,43 +1,52 @@
-import Style from './home.module.css';
-import Image from './../../assets/ll.jpg';
-import { GiGrapes } from 'react-icons/gi';
-import  Clock  from 'react-simple-clock';
-import Contacts from '../../components/contacts/contacts';
-import Portfolio from '../portfolio/portfolio';
+import Style from "./home.module.css";
+import Image from "./../../assets/ll.jpg";
+import { GiGrapes } from "react-icons/gi";
+import Clock from "react-simple-clock";
+import Contacts from "../../components/contacts/contacts";
+import Portfolio from "../portfolio/portfolio";
 
 export default function Home() {
-   
-    return(
-        <>
-       <div className={Style.containerName}>
-        <h1 className={Style.title}>
-            <GiGrapes/>
-            Livia Luciani
-            <GiGrapes/>
-        </h1>
-        </div>
+  return (
+    <>
+      <div className={Style.containerName}>
+        <GiGrapes size={30} />
+        <h1 className={Style.title}>Livia Luciani</h1>
+        <GiGrapes size={30} />
+      </div>
 
-        <div className={Style.container1}>
-            <Clock className={Style.clock} live={true} hourMarkFormat="number" mode="dark"/>
-           <div className={Style.ContainerText}> <p className={Style.text}>
-            Mi presento sono Livia, e sono junior front-end developer. <br /> Negli ultimi anni ho coltivato una grande passione per l’ informatica tramandata dalla mia famiglia. <br />
-            Per lungo tempo ho lavorato nel campo della ristorazione, <br /> 
-            diverse esperienze mi hanno insegnato la precisione, la puntualità e la determinazione, 
-            oltre alla conoscenza dei vini e dell&apos;alta ristorazione.<br/> 
-            Ma a 30 anni ho capito che volevo assecondare questo mio bisogno che è la tecnologia. <br/>
-            L&apos;idea di poter creare qualcosa che prima non c&apos;era mi rende felice. <br/> 
-            Così ho deciso di intraprendere gli studi nello sviluppo. <br/>
-            Vorrei poter unire la mie passioni nel digitale.<br/>
+      <div className={Style.container1}>
+        <Clock
+          className={Style.clock}
+          live={true}
+          hourMarkFormat="number"
+          mode="dark"
+        />
+        <div className={Style.ContainerText}>
+          {" "}
+          <p className={Style.text}>
+            Mi presento sono Livia, e sono junior front-end developer. <br />{" "}
+            Negli ultimi anni ho coltivato una grande passione per l’
+            informatica tramandata dalla mia famiglia. <br />
+            Per lungo tempo ho lavorato nel campo della ristorazione, <br />
+            diverse esperienze mi hanno insegnato la precisione, la puntualità e
+            la determinazione, oltre alla conoscenza dei vini e dell&apos;alta
+            ristorazione.
+            <br />
+            A 30 anni ho capito che volevo assecondare questo mio bisogno che è
+            la tecnologia. <br />
+            L&apos;idea di poter creare qualcosa che prima non c&apos;era mi
+            rende felice. <br />
+            Così ho deciso di intraprendere gli studi nello sviluppo. <br />
+            Vorrei poter unire la mie passioni nel digitale.
+            <br />
             Di seguito i miei progetti.
-      
-            </p>
-            </div>
-              <img className={Style.photo} src={Image} alt="photo-LL" />
+          </p>
         </div>
+        <img className={Style.photo} src={Image} alt="photo-LL" />
+      </div>
 
-        <Contacts/>
-        <Portfolio/>
-    
-        </>
-    )
+      <Contacts />
+      <Portfolio />
+    </>
+  );
 }
--- src/page/portfolio/portfolio.jsx
+++ src/page/portfolio/portfolio.jsx
@@ -1,39 +1,28 @@
-import Style from './portfolio.module.css';
-import Skills from '../../components/skills/skills';
-import { portfolioData } from './portfolioData';
-import { BsPersonWorkspace } from 'react-icons/bs';
- 
+import Style from "./portfolio.module.css";
+import Skills from "../../components/skills/skills";
+import { portfolioData } from "./portfolioData";
+import { BsPersonWorkspace } from "react-icons/bs";
 
 export default function Portfolio() {
-    return(
-        <>
-        <div className={Style.containerName}>
-        <h1 className={Style.title}>
-            <BsPersonWorkspace/>
-            Portfolio
-            <BsPersonWorkspace/>
-            </h1>
-            </div>
+  return (
+    <>
+      <div className={Style.containerName}>
+        <BsPersonWorkspace size={30} />
+        <h1 className={Style.title}>Portfolio</h1>
+        <BsPersonWorkspace size={30} />
+      </div>
 
-        <Skills/>
+      <Skills />
 
-        <div className={Style.containerSkills}>
-
-            {portfolioData.map(({id,image,description,link})=>(
-               <div key={id}>
-                 <a href={link}  target='_blank' rel='noreferrer'>
-                    <img className={Style.photo} src={image} alt={description} />
-                 </a>
-               </div> 
-            ))}
-
-        </div>   
-
-    
-
-       
-        </>
-    )
+      <div className={Style.containerSkills}>
+        {portfolioData.map(({ id, image, description, link }) => (
+          <div key={id}>
+            <a href={link} target="_blank" rel="noreferrer">
+              <img className={Style.photo} src={image} alt={description} />
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
-
-
